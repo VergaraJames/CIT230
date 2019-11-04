360c60c541aa27290abec602ad4ab192edea65e8
VergaraJames <ver18002@byui.edu>
2019-11-04 01:06:38 -0500

    commit;

--- lesson-7/js/main.js
+++ lesson-7/js/main.js
@@ -7,7 +7,7 @@ function toggleMenu() {
 function showhide() {
     var d = new Date();
     var s = document.getElementById(+d.getDay());
-    s.style.display = s.style.display == "block" ? "none" : "block";
+    s.style.display = (s.style.display == 'block') ? 'none' : 'block';
 }
 
 function wChill() {
@@ -34,70 +34,4 @@ function pancakes() {
         document.getElementById("pancakes").innerHTML =
             "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
     }
-}
-
-function pancakes() {
-    var day = new Date();
-    var weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
-
-    var today = weekday[day.getDay()];
-    if (today == "Friday") {
-        document.getElementById("pancakes").innerHTML =
-            "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-    }
-}
-
-function get_day() {
-    const options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-        hours: "numeric",
-        minutes: "numeric",
-        seconds: "numeric"
-    };
-    document.getElementById(
-        "currentdate"
-    ).textContent = new Date().toLocaleDateString("en-US", options);
-}
-
-function currentdate() {
-    const daynames = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ];
-    const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-    ];
-
-    const d = new Date();
-    const dayName = daynames[d.getDay()];
-    const monthName = months[d.getMonth()];
-    const year = d.getFullYear();
-    const fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
-    document.getElementById("currentdate").textContent = fulldate;
 }
