e92cd05c45720e2dc86529d29b376ebfd1a5dfa0
VergaraJames <ver18002@byui.edu>
2019-11-24 02:54:25 -0500

    commit;

--- lesson-10/js/apiweatherpreston.js
+++ lesson-10/js/apiweatherpreston.js
@@ -4,9 +4,9 @@ let units = 'imperial';
 const cityid = 5604473;
 
 // weatherAPIURL
-const weatherAPIURL = `https://api.openweathermap.org/data/2.5/weather?id=id=${cityid}&APPID=${appId}&units=${units}`;
+const weatherAPIURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&APPID=${appId}&units=${units}`;
 // forecastAPIURL
-const forecastAPIURL = `https://api.openweathermap.org/data/2.5/forecast?id=id=${cityid}&APPID=${appId}&units=${units}`;
+const forecastAPIURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityid}&APPID=${appId}&units=${units}`;
 const weatherIconPath = "https://openweathermap.org/img/w/";
 const dow = new Intl.DateTimeFormat("en-US", {
     weekday: "short"
@@ -19,12 +19,12 @@ fetch(weatherAPIURL)
         const tempF = jsObject.main.temp.toFixed(0);
         const windSpeedMPH = jsObject.wind.speed.toPrecision(2);
         const windChillF = windChill(tempF, windSpeedMPH);
-        document.getElementById('temperature').textContent = tempF;
-        document.getElementById('hightemp').textContent = jsObject.main.temp_max.toFixed(0);
-        document.getElementById('windspeed').textContent = windSpeedMPH;
-        document.getElementById('windchill').textContent = windChillF;
-        document.getElementById('humidity').textContent = jsObject.main.humidity.toFixed(0);
         document.getElementById('description').textContent = jsObject.weather[0].description;
+        document.getElementById('temperature').textContent = tempF;
+        document.getElementById('windchill').textContent = windChillF;
+        document.getElementById('hightemp').textContent = jsObject.main.temp_max.toFixed(0);
+        document.getElementById('humidity').textContent = jsObject.main.humidity.toFixed(0);
+        document.getElementById('windspeed').textContent = windSpeedMPH;
     });
 
 fetch(forecastAPIURL)
